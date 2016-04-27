7e479f16ae0b8d21a7193d1d4e3758f2f12ef9a9
JD <jeandaniel.michaud@ge.com>
2016-04-27 14:25:58 +0200

    Add the angular-translate mock object

---
+++ frontend/test/mock/angular-translate-mock.js
@@ -0,0 +1,24 @@
+define('mocks/translate-mock', function () {
+  'use strict';
+
+  // Translate mock
+  return function ($provide, value) {
+    var $translate = function (label) {
+      return {
+        then: function (callback) {
+          callback(label);
+          return {
+            catch: function (callback2) { },
+          };
+        },
+      };
+    };
+    $translate.storageKey = function () { return ''; };
+    $translate.storage = function () {
+      return { get: function () { return ''; } };
+    };
+    $translate.preferredLanguage = function () { return 'en'; };
+    $translate.use = function () { };
+    $provide.value('$translate', $translate);
+  };
+});
